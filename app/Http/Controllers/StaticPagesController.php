bf2cb98cc4ddbcc2af2e9ccb026cc36dfc734c8a
number5wh <1031158073@qq.com>
2019-02-12 10:44:00 +0800

    change

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    public function home() 
+    {
+
+    }
+
+    public function about()
+    {
+
+    }
+
+    public function help()
+    {
+    
+    }
+    
+}
