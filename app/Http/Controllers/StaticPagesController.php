60006c71a2b12f35ba1d922b806650d51b62eefc
number5wh <1031158073@qq.com>
2019-02-12 10:55:06 +0800

    static_pages

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -8,17 +8,17 @@ class StaticPagesController extends Controller
 {
     public function home() 
     {
-
+        return view('static_pages/home');
     }
 
     public function about()
     {
-
+        return view('static_pages/about');
     }
 
     public function help()
     {
-    
+        return view('static_pages/help');
     }
     
 }
